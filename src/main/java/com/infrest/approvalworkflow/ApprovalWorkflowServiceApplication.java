7aaff5fbab7385dced0583d903de48ac3dcaa30f
EmeDike <dicksoneme64@gmail.com>
2026-07-05 21:10:49 +0100

    chore: project scaffold - Maven, Spring Boot 3.3, Java 21

---
+++ src/main/java/com/infrest/approvalworkflow/ApprovalWorkflowServiceApplication.java
@@ -0,0 +1,12 @@
+package com.infrest.approvalworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalWorkflowServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalWorkflowServiceApplication.class, args);
+    }
+}
